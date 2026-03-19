d5805e27fe71f9aca928b0968248edd401adcaa2
Yashita16 <yashitagupta427@gmail.com>
2026-03-19 22:00:15 +0530

    feat: implement Home page with MainBanner and Categories components, update App component routing, and remove Products component

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainBanner from '../components/MainBanner.jsx'
+import Categories from '../components/Categories.jsx'
+
+const Home = () => {
+  return (
+    <div className='mt-10'>
+      <MainBanner />
+      <Categories/>
+    </div>
+  )
+}
+
+export default Home
